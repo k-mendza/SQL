4af68ead4edd65194249fb7e87e7e34090ed0201
k-mendza <35465634+k-mendza@users.noreply.github.com>
2018-06-04 11:17:24 +0200

    Add files via upload

--- AGGREGATE.sql
+++ AGGREGATE.sql
@@ -5,3 +5,45 @@ SELECT COUNT(DISTINCT author_fname) FROM books;
 SELECT COUNT(DISTINCT author_fname, author_lname) FROM books;
 -- count all books with title containing 'the'
 SELECT COUNT(*) FROM books WHERE title LIKE '%the%';
+-- count all books of each author
+SELECT 
+	author_fname,author_lname, COUNT(*) 
+FROM 
+	books
+GROUP BY 
+	author_lname,author_fname;
+-- count all books from the same year with ascending order of the released year
+SELECT
+	released_year, COUNT(*)
+FROM
+	books
+GROUP BY
+	released_year
+ORDER BY
+	released_year;
+-- find minimal year of release
+SELECT
+	MIN(released_year)
+FROM
+	books;
+-- find minimal number of pages
+SELECT
+	MIN(pages)
+FROM
+	books;
+-- find maximal number of pages
+SELECT
+	MAX(pages)
+FROM
+	books
+-- find the title of the book with maximal number of pages
+SELECT
+	title,pages
+FROM
+	books
+WHERE
+	pages=(
+		SELECT
+			MAX(pages)
+		FROM
+			books);
